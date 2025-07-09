5ff66741d6921b41202096e5e84008ae26084acb
Junner1st <jimmy.zm.7219@gmail.com>
2025-07-09 15:16:51 +0800

    1.0.1

--- src/main/java/replaceentities/ReplaceLogic.java
+++ src/main/java/replaceentities/ReplaceLogic.java
@@ -12,6 +12,13 @@ import net.minecraft.entity.mob.PillagerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.entity.SpawnReason;
+import net.minecraft.world.LocalDifficulty;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.ItemEnchantmentsComponent;
+import net.minecraft.entity.EquipmentSlot;
 
 public class ReplaceLogic implements ModInitializer {
     @Override
@@ -25,15 +32,37 @@ public class ReplaceLogic implements ModInitializer {
         });
     }
 
-    private void replaceEntity(ServerWorld world, LivingEntity original, EntityType<? extends LivingEntity> type) {
-        Vec3d pos = original.getPos();
-        float yaw = original.getYaw(), pitch = original.getPitch();
-        original.discard();
+    private static <T extends LivingEntity> T replaceEntity(ServerWorld world, LivingEntity orig, EntityType<T> type) {
+        var pos = orig.getPos();
+        var yaw = orig.getYaw();
+        var pitch = orig.getPitch();
+        orig.discard();
 
-		LivingEntity repl = type.create(world, SpawnReason.NATURAL);
-        if (repl == null) return;
+        T ent = type.create(world, SpawnReason.NATURAL);
+        if (ent == null) return null;
 
-        repl.refreshPositionAndAngles(pos.x, pos.y, pos.z, yaw, pitch);
-        world.spawnEntity(repl);
+        ent.refreshPositionAndAngles(pos.x, pos.y, pos.z, yaw, pitch);
+        
+        // equip mobs
+        if (ent instanceof VindicatorEntity vindicator) {
+            equipVindicator(vindicator, world);
+        } else if (ent instanceof PillagerEntity pillager) {
+            equipPillager(pillager, world);
+        }
+        
+        world.spawnEntity(ent);
+        return ent;
+    }
+    
+    private static void equipVindicator(VindicatorEntity vindicator, ServerWorld world) {
+        ItemStack axe = new ItemStack(Items.IRON_AXE);
+        vindicator.equipStack(EquipmentSlot.MAINHAND, axe);
+        vindicator.setEquipmentDropChance(EquipmentSlot.MAINHAND, 0.05f);
+    }
+    
+    private static void equipPillager(PillagerEntity pillager, ServerWorld world) {
+        ItemStack crossbow = new ItemStack(Items.CROSSBOW);
+        pillager.equipStack(EquipmentSlot.MAINHAND, crossbow);
+        pillager.setEquipmentDropChance(EquipmentSlot.MAINHAND, 0.05f);
     }
 }
